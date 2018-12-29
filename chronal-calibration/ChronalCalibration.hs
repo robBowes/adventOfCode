11828502750bf6e84ea793cfe5256cc6c34b9317
robBowes <32183505+robBowes@users.noreply.github.com>
2018-12-28 23:30:44 -0500

    add some comments

--- chronal-calibration/ChronalCalibration.hs
+++ chronal-calibration/ChronalCalibration.hs
@@ -1,21 +1,29 @@
+-- Challenge 1: Find the final frequency after changing the dial as directed in the input.txt
+-- Challenge 2: Find the first time there is a repeated freuency when changing the dial as directed, the input can be repeated
+
 main = do
     inputStr<-readFile "input.txt"
+    -- parse the input into a list of strings
     let listOfStr = lines inputStr
+    -- parse the input into a list of integers
     let listOfFrequencies = map strToInt listOfStr
+    -- find the sum of the integers
     let sumOfFrequencies = sum listOfFrequencies
     putStrLn $ "The resulting frequency after all changes was " ++ show sumOfFrequencies
+    -- use findRepeat to find the first repeat given the list of freqencies
     let firstRepeat = findRepeat listOfFrequencies listOfFrequencies 0 [0]
     putStrLn $ "The first repeated frequency was " ++ show firstRepeat
 
+-- use these for testing
 test' = [3,3,4,-2,-4]
 test'' = [-6,3,8,5,-6]
 test3 = [7,7,-2,-7,-4]
 
-
+-- strToInt parses the values from the advent of code input
 strToInt::String->Integer
 strToInt (x:xs) = if x == '+' then read xs else read $ x:xs
 
--- original array, remaining list of frequencies, current frequency, list of results return repeat frequency
+-- params: original array, remaining list of frequencies, current frequency, list of results return repeat frequency
 findRepeat::[Integer]->[Integer]->Integer->[Integer]->Integer
 -- if the remaining frequencies are empty, restart the iteration at the begining
 findRepeat frequencies [] currentFrequency pastFrequencies = findRepeat frequencies frequencies currentFrequency pastFrequencies
