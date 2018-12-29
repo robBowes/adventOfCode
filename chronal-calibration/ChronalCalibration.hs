26399f116a019f3cd4884bd5ef5d09dc82d5bc40
robBowes <32183505+robBowes@users.noreply.github.com>
2018-12-28 23:13:01 -0500

    day 1 part 2

--- chronal-calibration/ChronalCalibration.hs
+++ chronal-calibration/ChronalCalibration.hs
@@ -3,8 +3,25 @@ main = do
     let listOfStr = lines inputStr
     let listOfFrequencies = map strToInt listOfStr
     let sumOfFrequencies = sum listOfFrequencies
-    putStrLn $ show sumOfFrequencies
+    putStrLn $ "The resulting frequency after all changes was " ++ show sumOfFrequencies
+    let firstRepeat = findRepeat listOfFrequencies listOfFrequencies 0 [0]
+    putStrLn $ "The first repeated frequency was " ++ show firstRepeat
+
+test' = [3,3,4,-2,-4]
+test'' = [-6,3,8,5,-6]
+test3 = [7,7,-2,-7,-4]
 
 
-strToInt::String->Int
+strToInt::String->Integer
 strToInt (x:xs) = if x == '+' then read xs else read $ x:xs
+
+-- original array, remaining list of frequencies, current frequency, list of results return repeat frequency
+findRepeat::[Integer]->[Integer]->Integer->[Integer]->Integer
+-- if the remaining frequencies are empty, restart the iteration at the begining
+findRepeat frequencies [] currentFrequency pastFrequencies = findRepeat frequencies frequencies currentFrequency pastFrequencies
+findRepeat frequencies (nextFrequency:remainingFrequencies) currentFrequency pastFrequencies = do
+    -- Add the head of the frequencies to the accumulator
+    let result = currentFrequency + nextFrequency
+    if result `elem` pastFrequencies 
+        then result 
+        else findRepeat frequencies remainingFrequencies result (result:pastFrequencies)
